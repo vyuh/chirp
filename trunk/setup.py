f5a687e64aac077c795211570adafc701a287a80
illicium <illicium@69ec188d-232c-0410-9e33-3d0f4e308997>
2007-04-01 20:37:32 +0000

    Added .desktop file & icon, setup.py fixes

--- trunk/setup.py
+++ trunk/setup.py
@@ -1,17 +1,24 @@
+#!/usr/bin/env python
 import ez_setup
 ez_setup.use_setuptools()
-import setuptools
+import setuptools, glob
+
+data = [('share/chirp/glade',  glob.glob('glade/*.glade')),
+        ('share/applications' , ['chirp.desktop']),
+        ('share/pixmaps' , ['chirp.png'])]
 
 setuptools.setup (
 	name='chirp',
 	version='0.1',
         scripts=['scripts/chirp'],
         packages=['chirp'],
-        package_dir = {'chirp': 'src'},
+        package_dir={'chirp': 'src'},
+        data=data,
+        ext_package='chirp',
 	author='George Pomortsev',
 	author_email='illicium@gmail.com',
 	description='Twitter desktop client',
-	long_description='A desktop client for reading and submitting messages to Twitter',
+	long_description='Python/GTK+ Twitter desktop client',
 	license='New BSD License',
 	url='http://code.google.com/p/chirp',
 	keywords='twitter desktop client gtk python',
